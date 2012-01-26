165c71fae4f90503b7e56f9782c6790b9b387775
Normandes Junior <normandes.junior@algaworks.com>
2012-01-25 22:04:47 -0200

    Codigo fonte da aula trabalhando com variaveis

---
+++ Aulas/Trabalhando com variaveis/Variavel1.java
@@ -0,0 +1,14 @@
+public class Variavel1 {
+
+	public static void main(String[] args) {
+		int quantidade; // Declarando variavel inteiro
+		quantidade = 10; // atribuindo o valor 10
+		
+		System.out.println(quantidade);
+		
+		quantidade = 15;
+		
+		System.out.println(quantidade);		
+	}
+
+}
