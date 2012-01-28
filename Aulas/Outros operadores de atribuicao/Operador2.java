84946b269c775eea767cf4da860f8df62f672186
Normandes Junior <normandes.junior@algaworks.com>
2012-01-28 10:34:55 -0200

    Codigo fonte da aula outros operadores de atribuicao

---
+++ Aulas/Outros operadores de atribuicao/Operador2.java
@@ -0,0 +1,17 @@
+public class Operador2 {
+
+	public static void main(String[] args) {
+		int total = 10;
+		total += 3;
+		System.out.println(total); // imprime 13
+		total -= 2;
+		System.out.println(total); //imprime 11
+		total *= 3;
+		System.out.println(total); //imprime 33
+		total /= 3;
+		System.out.println(total); // imprime 11
+		total %= 5;
+		System.out.println(total); // imprime 1
+	}
+
+}
