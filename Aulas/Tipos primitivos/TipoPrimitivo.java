648d5cc11141e90feaa70984216a58c673a52f29
Normandes Junior <normandes.junior@algaworks.com>
2012-01-28 10:31:10 -0200

    Codigo fonte da aula tipos primitivos

---
+++ Aulas/Tipos primitivos/TipoPrimitivo.java
@@ -0,0 +1,22 @@
+public class TipoPrimitivo {
+
+	public static void main(String[] args) {
+		double precoProduto = 9.43;
+		
+		boolean alunoMatriculado = true;
+		boolean clienteBloqueado = false;
+		
+		char turmaAluno1 = 'A';
+		char tipoCliente = '2';
+		char simbolo = '@';
+		
+		long populacaoUberlandia = 650000;
+		System.out.println(populacaoUberlandia);
+		
+		long populacaoMundial = 7000000000L;
+		System.out.println(populacaoMundial);
+		
+		float saldoConta = 1030.43F;
+		System.out.println(saldoConta);
+	}
+}
