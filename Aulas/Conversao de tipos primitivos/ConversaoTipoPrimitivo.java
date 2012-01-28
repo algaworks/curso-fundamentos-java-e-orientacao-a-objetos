344129eb45ff19911d7367651b12103d340b3069
Normandes Junior <normandes.junior@algaworks.com>
2012-01-28 10:40:15 -0200

    Codigo fonte da aula conversao de tipos primitivos

---
+++ Aulas/Conversao de tipos primitivos/ConversaoTipoPrimitivo.java
@@ -0,0 +1,12 @@
+public class ConversaoTipoPrimitivo {
+
+	public static void main(String[] args) {
+		double largura = 100.37;
+		
+		int tamanho = (int) largura;
+		
+		System.out.println(tamanho);
+	}
+
+
+}
