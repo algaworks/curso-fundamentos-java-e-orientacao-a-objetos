5ad70d51ee370cb204203be6c6ef5dc1bf0552a3
normandes.junior@algaworks.com <normandes.junior@algaworks.com>
2012-01-28 10:25:20 -0200

    Codigo fonte da aula Operadores Aritmeticos

---
+++ Aulas/Operadores aritmeticos/OperadorAritmetico1.java
@@ -0,0 +1,15 @@
+public class OperadorAritmetico1 {
+
+	public static void main(String[] args) {
+		int notaAluno1 = 99;
+		int notaAluno2 = 80;
+		int notaAluno3 = 53;
+		
+		int totalGeral = notaAluno1 + notaAluno2 + notaAluno3;
+		System.out.println("Total geral: " + totalGeral);
+		
+		int mediaGeral = (notaAluno1 + notaAluno2 + notaAluno3) / 3;
+		System.out.println("Media geral: " + mediaGeral);
+	}
+
+}
