8894baa7c1fe134b20db4923aec1ff161dca5622
Normandes Junior <normandes.junior@algaworks.com>
2012-02-06 21:18:46 -0200

    Inclusao de codigos de aula

---
+++ Aulas/Organizando o projeto em pacotes/Cachorro.java
@@ -0,0 +1,16 @@
+package com.algaworks.curso.fjoo.animal;
+
+public class Cachorro {
+
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+	
+}
---
+++ Aulas/Organizando o projeto em pacotes/TreinadorCachorro.java
@@ -0,0 +1,12 @@
+package com.algaworks.curso.fjoo.treinador;
+
+import com.algaworks.curso.fjoo.animal.Cachorro;
+
+public class TreinadorCachorro {
+
+	public static void main(String[] args) {
+		Cachorro cachorro = new Cachorro();
+		cachorro.setNome("Bob");
+	}
+
+}
