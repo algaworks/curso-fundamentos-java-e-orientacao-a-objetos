3b978cbe2ccfdb6d60aa0c2cd59197c33594c84b
Thiago Faria de Andrade <thiagofa@gmail.com>
2012-02-12 00:22:14 -0200

    Exercício sobre interfaces e polimorfismo

---
+++ Exercicios/Interfaces e polimorfismo/CorretoraSeguros.java
@@ -0,0 +1,12 @@
+public class CorretoraSeguros {
+
+	public void fazerPropostaSeguro(Seguravel objetoSeguravel) {
+		System.out.println("-------------------------------");
+		System.out.println("Corretora de Seguros - Proposta");
+		System.out.println("-------------------------------");
+		System.out.println(objetoSeguravel.obterDescricao());
+		System.out.println("Valor da apólice: " + objetoSeguravel.calcularValorApolice());
+		System.out.println("-------------------------------\n");
+	}
+
+}
