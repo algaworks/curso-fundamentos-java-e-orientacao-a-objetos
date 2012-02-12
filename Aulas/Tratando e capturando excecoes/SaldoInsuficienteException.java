c317be8a0a96e8913b2ddc6c4601a0d9c374b93d
Thiago Faria de Andrade <thiagofa@gmail.com>
2012-02-12 00:51:25 -0200

    Removendo diretorio duplicado

--- Aulas/Tratando e capturando excecoes/SaldoInsuficienteException.java
+++
@@ -1,9 +0,0 @@
-package com.algaworks.curso.fjoo.excecoes;
-
-public class SaldoInsuficienteException extends Exception {
-
-	public SaldoInsuficienteException(String mensagem) {
-		super(mensagem);
-	}
-	
-}
