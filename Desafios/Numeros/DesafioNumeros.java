a6e239a9062f46be14ea9eacffc9d7db8f1e34bc
Thiago Faria de Andrade <thiagofa@gmail.com>
2012-02-13 23:36:31 -0200

    Desafio sobre números

---
+++ Desafios/Numeros/DesafioNumeros.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.ParseException;
+
+public class DesafioNumeros {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		DecimalFormat formatador = new DecimalFormat("R$ #,##0.00");
+		
+		System.out.println("Valor do produto:");
+		double valor = entrada.nextDouble();
+		
+		BigDecimal valorBig = new BigDecimal(valor);
+		BigDecimal resultado = valorBig.multiply(BigDecimal.TEN).divide(new BigDecimal(100));
+		
+		String valorFormatado = formatador.format(resultado.doubleValue());
+		System.out.println("10% do valor é: " + valorFormatado);
+	}
+
+}
