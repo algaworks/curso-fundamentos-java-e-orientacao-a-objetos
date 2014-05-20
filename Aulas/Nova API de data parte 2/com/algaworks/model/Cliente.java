860ac79f3b03145a627a5f58369516cdd54a06a6
Normandes Junior <normandesjr@gmail.com>
2014-05-20 17:11:08 -0300

    Nova API de data parte 2

---
+++ Aulas/Nova API de data parte 2/com/algaworks/model/Cliente.java
@@ -0,0 +1,31 @@
+package com.algaworks.model;
+
+import java.time.LocalDate;
+
+public class Cliente {
+
+	private String nome;
+	private LocalDate dataNascimento;
+
+	public Cliente(String nome, LocalDate dataNascimento) {
+		this.nome = nome;
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+}
